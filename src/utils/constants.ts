035b20e70defade2739e3afb18af744affe327d7
Kit Allen <kitallen23@gmail.com>
2025-05-01 19:31:37 +1000

    Chore: bumped version number

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const HOMEPAGE = "/";
 export const LOCAL_STORAGE_PREFIX = "gw2-tools";
 export const GITHUB_URL = "https://github.com/kitallen23/gw2-tools";
-export const APP_VERSION = "0.3.1";
+export const APP_VERSION = "0.3.2";
